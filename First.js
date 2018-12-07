97f2e6ff4149d89d97610076bb3c7555683cadcb
Harish Bathina <harish.bathina@ggktech.local>
2018-12-07 17:21:07 +0530

    second

--- First.js
+++ First.js
@@ -1,11 +1,18 @@
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, Button, WebView, TextInput, FlatList, View } from 'react-native';
 
 export default class First extends Component {
     constructor(props) {
         super(props);
         this.state = { isShowing: true, text: 'Before onChange' };
+        // setInterval(() => {
+        //     this.setState(previousState => (
+        //         {
+        //             isShowing: !previousState.isShowing
+        //         }
+        //     ))
+        // }, 1000);
     }
 
     textChange(text) {
@@ -14,23 +21,50 @@ export default class First extends Component {
         });
     }
 
+
+
+
+
     render() {
+        let value = [];
         if (this.state.isShowing) {
             return (
-                <View style={styles.container}>
+                <ScrollView style={styles.container}>
                     <Text>First {this.props.name} component</Text>
                     <TextInput placeholder="Type here" onChangeText={(text) => this.setState({ text })}></TextInput>
                     <Text>{this.state.text}</Text>
                     <Button color='#841584' onPress={() => {
-                        alert("You tapped the button")
+                        fetch('https://facebook.github.io/react-native/movies.json')
+                            .then((response) => response.json())
+                            .then((responseJson) => {
+                                value = responseJson.movies;
+                                alert(JSON.stringify(value));
+                            })
+                            .catch((error) => {
+                                alert(error);
+                            });
                     }} title="Tap Me"></Button>
-                </View>
+
+                    <FlatList
+                        data={[
+                            { key: 'Devin' },
+                            { key: 'Jackson' },
+                            { key: 'James' },
+                            { key: 'Joel' },
+                            { key: 'John' },
+                            { key: 'Jillian' },
+                            { key: 'Jimmy' },
+                            { key: 'Julie' },
+                        ]}
+                        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+                    />
+                </ScrollView>
+                // <WebView source={{ uri: 'https://www.youtube.com/embed/ChGrinBQbE0' }} />
             );
         }
         else {
             return null;
         }
-
     }
 }
 
@@ -39,5 +73,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#7fffd4',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
     }
 })
